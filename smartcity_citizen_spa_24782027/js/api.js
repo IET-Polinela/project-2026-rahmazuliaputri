27d97d35b92cc043866998ca69360b2f512868a6
rahmazuliaputri <rahmazuliaputri29@gmail.com>
2026-06-13 23:30:37 +0700

    LabSession13 GitHub Pages deployment

--- smartcity_citizen_spa_24782027/js/api.js
+++ smartcity_citizen_spa_24782027/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'http://103.151.63.84:8009';
 
 async function refreshAccessToken() {
     const refreshToken = localStorage.getItem('refresh_token');
